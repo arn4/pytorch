1a13a662906c0c18b51ad1c2f39f0233412721a5
Aditya Tewari <aditya.tewari@arm.com>
2024-11-08 18:02:38 +0000

    cpu: aarch64: enable gemm-bf16f32

--- aten/src/ATen/native/CPUBlas.cpp
+++ aten/src/ATen/native/CPUBlas.cpp
@@ -326,7 +326,6 @@ void gemm(
    if (use_blas_gemm(transa, transb, m, n, k, lda, ldb, ldc)) {
       int m_ = m, n_ = n, k_ = k, lda_ = lda, ldb_ = ldb, ldc_ = ldc;
       char transa_ = to_blas(transa), transb_ = to_blas(transb);
-      float alpha_ = alpha, beta_ = beta;
       int c_size = n_ * ldc_;
       // C matrix in OpenBLAS sbgemm are of type "float" so we have to convert, copy and copy back.
       std::vector<float> float_v(c, c + c_size);
@@ -344,9 +343,6 @@ void gemm(
    }
 #endif
 #if AT_MKLDNN_ENABLED()
-#ifdef __aarch64__
-   // MKLDNN also supports ARM for bf16, and the bypass is only
-   // currently intended for x86/x86_64.
    const bool use_bf16_gemv_trans = false;
 #elif defined(__powerpc__)
    const bool use_bf16_gemv_trans = false;
@@ -408,6 +404,13 @@ void gemm(
     const float beta,
     float *c, int64_t ldc) {
   internal::normalize_last_dims(transa, transb, m, n, k, &lda, &ldb, &ldc);
+
+// TODO: ADI: add heuristic based on shape to dispatch to sbgemm_ vs MKLDNN
+#if AT_MKLDNN_ENABLED()
+   if (mkldnn_bf16f32_gemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)) {
+     return;
+   }
+#endif
 #if AT_BUILD_WITH_BLAS() && defined(BLAS_HAS_SBGEMM)
    if (use_blas_gemm(transa, transb, m, n, k, lda, ldb, ldc)) {
       int m_ = m, n_ = n, k_ = k, lda_ = lda, ldb_ = ldb, ldc_ = ldc;
